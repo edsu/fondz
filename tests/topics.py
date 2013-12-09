bb5027f73513a6d925153ea94cd6d56c7c8cb165
Ed Summers <ehs@pobox.com>
2013-12-09 15:04:40 -0500

    added some sample data for tests to play with

--- tests/topics.py
+++ tests/topics.py
@@ -2,9 +2,12 @@ from unittest import TestCase
 
 import diagnidnif
 
+test_data = os.path.join(os.path.dirname(__name__), 'data')
+keats = os.path.join(test_data, 'keats')
+
 class TopicTest(TestCase):
     
     def test_topics(self):
-        topics = diagnidnif.mallet.topics()
+        topics = diagnidnif.mallet.topics(keats)
         self.assertEqual(topics, ["foo", "bar"])
 
