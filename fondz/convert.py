c0df74e6e237752d14c786da17741a86b1e5130f
Ed Summers <ehs@pobox.com>
2013-12-18 00:13:49 +0000

    added tests and implementation for fondz.convert.convert_dir

--- fondz/convert.py
+++ fondz/convert.py
@@ -1,10 +1,30 @@
 import os
+import logging
 import tempfile
 
 from utils import run
 
-def convert(src_dir, target_dir):
-    return True
+def convert_dir(src_dir, target_dir):
+
+    for filename in os.listdir(src_dir):
+        path = os.path.join(src_dir, filename)
+
+        if not os.path.isdir(target_dir):
+            os.mkdir(target_dir)
+
+        if os.path.isdir(path):
+            convert_dir(path, os.path.join(target_dir, filename))
+
+        # try to convert the file to html
+        tmp_file = convert_to_html(path)
+        if not os.path.isfile(tmp_file):
+            continue
+
+        # move the temporary html file to the target_dir
+        # preserving the path within the src_dir
+        new_file = os.path.join(target_dir, filename + ".html")
+        logging.info("moving %s to %s", tmp_file, new_file)
+        os.rename(tmp_file, new_file)
 
 def convert_to_html(path):
     tmp_dir = tempfile.mkdtemp()
@@ -20,5 +40,6 @@ def convert_to_html(path):
     ]
     rc = run(cmd)
     if rc != 0:
-        raise Exception("unable to convert %s" % path)
+        logging.error("convert to html returned non zero: %i", rc)
+        return None
     return html_file
--- tests/convert.py
+++ tests/convert.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 from unittest import TestCase
 
-from fondz.convert import convert, convert_to_html
+from fondz.convert import convert_dir, convert_to_html
 
 test_data = os.path.join(os.path.dirname(__file__), 'data', 'convert')
 
@@ -20,7 +20,17 @@ class ConvertTest(TestCase):
         self.assertTrue(f2)
         self.assertTrue(os.path.isfile(f2))
 
-
-
+    def test_convert_dir(self):
+        target_dir = tempfile.mkdtemp() 
+        convert_dir(test_data, target_dir)
+        files = os.listdir(target_dir)
+        self.assertEqual(len(files), 3)
+        self.assertTrue('word.doc.html' in files)
+        self.assertTrue('wordperfect.wp.html' in files)
+        self.assertTrue('subdir' in files)
+        subdir = os.path.join(target_dir, 'subdir')
+        files = os.listdir(subdir)
+        self.assertEqual(len(files), 1)
+        self.assertTrue('word.docx.html' in files)
 
 
