6d8e249a4cf7a55de3f6b0cabfe6749c2930dd8e
Ed Summers <ehs@pobox.com>
2013-12-19 03:20:11 +0000

    beefed up identification tests a bit

--- fondz/identify.py
+++ fondz/identify.py
@@ -5,11 +5,13 @@ mediatype = magic.Magic(mime=True)
 description = magic.Magic()
 
 def identify_dir(src_dir):
+    src_dir = os.path.abspath(src_dir)
+    parent_dir = os.path.dirname(src_dir)
     formats = []
     for dirpath, dirnames, filenames in os.walk(src_dir):
         for filename in filenames:
             path = os.path.join(dirpath, filename)
-            rel_path = os.path.relpath(path, src_dir)
+            rel_path = os.path.relpath(path, parent_dir)
             f_mediatype = mediatype.from_file(path)
             f_desc = description.from_file(path)
             formats.append({
--- tests/identify.py
+++ tests/identify.py
@@ -9,4 +9,18 @@ class FileFormat(unittest.TestCase):
     def test_identify_dir(self):
         formats = fondz.identify.identify_dir(test_data)
         self.assertEqual(len(formats), 4)
-        #import json; print json.dumps(formats, indent=2)
+        for f in formats:
+            if f['path'] == 'data/wordperfect.wp':
+                self.assertEqual(f['mediatype'], 'application/octet-stream')
+                self.assertEqual(f['description'], '(Corel/WP)')
+            elif f['path'] == 'data/newspaper.jpg':
+                self.assertEqual(f['mediatype'], 'image/jpeg')
+                self.assertEqual(f['description'], 'JPEG image data, JFIF standard 1.01')
+            elif f['path'] == 'data/subdir/word.docx':
+                self.assertEqual(f['mediatype'], 'application/msword')
+                self.assertEqual(f['description'], 'Microsoft Word 2007+')
+            elif f['path'] == 'data/word.doc':
+                self.assertEqual(f['mediatype'], 'application/msword')
+                self.assertEqual(f['description'], "Composite Document File V2 Document, Little Endian, Os: Windows, Version 5.0, Code page: 1252, Title: _, Template: Normal.dot, Last Saved By:  , Revision Number: 6, Name of Creating Application: Microsoft Word 10.0, Last Printed: Wed Aug 24 20:41:00 2005, Create Time/Date: Thu Aug 25 20:50:00 2005, Last Saved Time/Date: Thu Aug 25 20:54:00 2005, Number of Pages: 2, Number of Words: 297, Number of Characters: 1648, Security: 0")
+            else:
+                self.fail("unexpected path: %s" % f['path'])
